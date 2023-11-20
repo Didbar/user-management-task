57a622a0425b0037160b186df694e313e7985386
Daviti Didbaridze <ddidbaridze@gmail.com>
2023-11-20 14:09:26 +0100

    clear initial data

--- src/api/groups.ts
+++ src/api/groups.ts
@@ -1,26 +1,7 @@
 import USERS from 'src/api/users'
 import Group from 'src/entities/Group'
 
-const GROUPS: Group[] = [
-  {
-    id: 1,
-    name: 'Community Outreach Specialist',
-    members: [1, 2],
-    slug: 'et',
-  },
-  {
-    id: 2,
-    name: 'Social Worker',
-    members: [1, 3],
-    slug: 'ridiculus',
-  },
-  {
-    id: 3,
-    name: 'Staff Accountant IV',
-    members: [1, 2, 3],
-    slug: 'arcu',
-  },
-]
+const GROUPS: Group[] = []
 
 export const getGroups = () => {
   return new Promise<Group[]>(resolve => resolve(GROUPS))
--- src/api/users.ts
+++ src/api/users.ts
@@ -1,26 +1,7 @@
 import GROUPS, { addUserToGroups } from 'src/api/groups'
 import User from 'src/entities/User'
 
-const USERS: User[] = [
-  {
-    id: 1,
-    name: 'Daisey Mennell',
-    email: 'dmennell0@hexun.com',
-    groups: [1, 2, 3],
-  },
-  {
-    id: 2,
-    name: 'Sherlock Shaughnessy',
-    email: 'sshaughnessy1@wordpress.com',
-    groups: [1, 3],
-  },
-  {
-    id: 3,
-    name: 'Timmie Formby',
-    email: 'tformby2@cbslocal.com',
-    groups: [2, 3],
-  },
-]
+const USERS: User[] = []
 
 export const getUsers = () => {
   return new Promise<User[]>(resolve => resolve(USERS))
